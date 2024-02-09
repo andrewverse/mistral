0ee91de7b12ed45851aac36defbdd6c01dcb964c
giordanorogers <giordanopaulrogers@gmail.com>
2024-02-09 11:03:24 -0500

    Built out the models and chains. Still prototying with OpenAI until we get the fine-tuned mistral model working

---
+++ models/mainXChain.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+import pandas as pd
+from langchain.chains import SequentialChain
+from summaryChain import summ_chain
+from ideaGenChain import idea_chain
+from tweetWriterChain import tweet_chain
+from userProfile import user_profile
+from articles import article
+
+load_dotenv()
+
+# create chain
+overall_chain = SequentialChain(
+    chains=[summ_chain, idea_chain, tweet_chain],
+    input_variables=["article", "user_profile"],
+    output_variables=["summary", "tweetIdeas","tweetDrafts"],
+    verbose=True
+)
+
+# invoke chain
+articleToTweets = overall_chain.invoke(
+    {
+        "article": article,
+        "user_profile": user_profile
+    }
+)
+
+print(articleToTweets)
---
+++ models/tweetWriterChain.py
@@ -0,0 +1,49 @@
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from userProfile import user_profile
+from ideaGenChain import tweetIdeas
+
+load_dotenv()
+
+# create a template
+tweet_template = """
+    Choose the 3 most compelling tweet ideas from the following list:
+    {tweetIdeas}
+    ---
+    Revise them for clarity and impact.
+    ---
+    Keep the tweets relevant to the following user profile:
+    {user_profile}
+"""
+
+# assign template to prompt
+tweet_prompt = ChatPromptTemplate.from_template(tweet_template)
+
+# declare a model
+tweet_model = (
+    ChatOpenAI(temperature=0.8)
+)
+
+# declare an output parser
+tweet_output_parser = StrOutputParser()
+
+# setup chain
+tweet_chain = (
+    {"tweetIdeas": RunnablePassthrough(), "user_profile": RunnablePassthrough()}
+    | tweet_prompt
+    | tweet_model
+    | tweet_output_parser
+)
+
+# invoke chain
+tweetDrafts = tweet_chain.invoke(
+    {
+        "tweetIdeas": tweetIdeas,
+        "user_profile": user_profile
+    }
+)
+
+print(tweetDrafts)
